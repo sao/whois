3af5aa05ac726704b949909b95391b3bd250931c
Silas Sao <silassao@gmail.com>
2014-04-30 16:30:20 -0700

    add ability to register through dnsimple, move around methods a bit

--- app.rb
+++ app.rb
@@ -1,3 +1,26 @@
 require 'sinatra'
 require 'rest_client'
 require 'whois'
+require 'dnsimple'
+require 'dotenv'
+
+Dotenv.load
+
+def send_email email, domain
+  RestClient.post "https://api:#{ENV['mailgun_api_key']}"\
+    "@api.mailgun.net/v2/sandbox1cd1bda3ce10419b949ba4746e3546d2.mailgun.org/messages",
+    :from => "Mailgun Sandbox <postmaster@sandbox1cd1bda3ce10419b949ba4746e3546d2.mailgun.org>",
+    :to => email,
+    :subject => "#{domain} has moved out of pendingDelete status",
+    :text => "you know what this is you bitch"
+end
+
+def register_domain domain
+  DNSimple::Client.username = ENV['dnsimple_username']
+  DNSimple::Client.api_token = ENV['dnsimple_api_token']
+  DNSimple::Client.http_proxy = {}
+
+  registrant = DNSimple::Contact.all.first.id
+
+  DNSimple::Domain.register(domain, { id: registrant })
+end
--- lib/tasks/whois.rake
+++ lib/tasks/whois.rake
@@ -4,15 +4,7 @@ namespace :whois do
     d = Whois.whois(args[:domain_name])
     if d.match('pendingDelete').nil?
       send_email ENV['contact_email'], args[:domain_name]
+      register_domain args[:domain_name]
     end
   end
-end
-
-def send_email email, domain
-  RestClient.post "https://api:#{ENV['mailgun_api_key']}"\
-    "@api.mailgun.net/v2/sandbox1cd1bda3ce10419b949ba4746e3546d2.mailgun.org/messages",
-    :from => "Mailgun Sandbox <postmaster@sandbox1cd1bda3ce10419b949ba4746e3546d2.mailgun.org>",
-    :to => email,
-    :subject => "#{domain} has moved out of pendingDelete status",
-    :text => "you know what this is you bitch"
 end
